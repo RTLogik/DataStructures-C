a169d4c41eb5dd0b01fb94c5482ad4d96a544fa6
RTLogik <antoniogn97@gmail.com>
2021-09-09 18:01:28 +0200

    changed "lifo" to "stack"

---
+++ include/stack.h
@@ -0,0 +1,44 @@
+/**
+ * @file stack.h
+ * @author rtlogik - contact@rtlogik.com
+ * @brief Stack Interface Header
+ * 
+ */
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+/**
+ * Defines the maximum size of the stack buffer
+ */
+#define MAX_STACK_SIZE 100
+
+/**
+ * @brief Stack Status Enumeration
+ * 
+ * This enumeration is used as a return value for all of the functions
+ * 
+ */
+typedef enum {
+    STACK_OK = 0,
+    STACK_INIT_ERROR,
+    STACK_EMPTY,
+    STACK_NOT_EMPTY,
+    STACK_FULL,
+    STACK_NOT_FULL,
+    STACK_NULL
+} StackStatus_e;
+
+typedef struct StackStruct *Stack_t;
+
+/************** STACK INTERFACE **************/
+StackStatus_e Stack_Init(Stack_t buffer, int length); // Constructor
+StackStatus_e Stack_Is_Full(Stack_t buffer);
+StackStatus_e Stack_Is_Empty(Stack_t buffer);
+StackStatus_e Stack_Push(Stack_t buffer, int item2push);
+StackStatus_e Stack_Pop(Stack_t buffer, int * item2pop);
+StackStatus_e Stack_Delete(Stack_t buffer);  // Destructor
+// peek() - only get the upper element (without popping)
+// size() - returns the size of the buffer
+
+#endif /* _STACK_H_ */
