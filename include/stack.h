59e0c947905389883f240d7ab53e34551df26ab4
rtlogik <antoniogn97@gmail.com>
2021-09-30 17:29:27 +0200

    updated stack API

--- include/stack.h
+++ include/stack.h
@@ -23,6 +23,7 @@ typedef enum {
     STACK_OK = 0,
     STACK_INIT_ERROR,
     STACK_LENGTH_ERROR,
+    STACK_INDEX_ERROR,
     STACK_EMPTY,
     STACK_NOT_EMPTY,
     STACK_FULL,
@@ -35,12 +36,14 @@ typedef struct StackStruct *Stack_t;
 
 /************** STACK INTERFACE **************/
 StackStatus_e Stack_Init(Stack_t *bufferPtr, int length, int *memory); 
-void Stack_Delete(Stack_t buffer);            
+void Stack_Delete(Stack_t buffer);   
+StackStatus_e Stack_Clear(Stack_t buffer);      
 StackStatus_e Stack_Is_Full(Stack_t buffer);
 StackStatus_e Stack_Is_Empty(Stack_t buffer);
 StackStatus_e Stack_Push(Stack_t buffer, int item2push);
 StackStatus_e Stack_Pop(Stack_t buffer, int *item2pop);
 StackStatus_e Stack_Peek(Stack_t buffer, int *item2peek);
+StackStatus_e Stack_Peek_Index(Stack_t buffer, int *item2peek, int index);
 int Stack_Size(Stack_t buffer);
 
 #endif /* _STACK_H_ */
